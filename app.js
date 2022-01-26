bdb815f3e9e955ff0e06c3bf363ec007d424d971
jethro revalde <jethro.revalde@hotmail.com>
2022-01-26 14:38:39 +1100

    just a simple restful API. Will make a more entertaining one later

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const internal = require("stream");
 
 app.use(express.urlencoded({extended: true}));
 
@@ -37,8 +38,10 @@ app.post("/", function(req, res){
 
 })
 
-app.get("/article", function(req, res){
-    console.log("Deez Yummy Nutz");
+//article route chaining
+
+app.route("/article")
+.get(function(req,res){
     Article.find(function(err, result){
         if(err)
         {
@@ -47,15 +50,100 @@ app.get("/article", function(req, res){
         else
         {
             console.log(result);
+            res.send(result);
         }
        
+    });
+})
+.post(function(req,res){
+    var title_post = req.body.title;
+    var title_content = req.body.content;
+
+    console.log(title_post);
+    console.log(title_content);
+
+    const newArticle = new Article({
+        title: title_post,
+        content: title_content
     })
 
+    newArticle.save(function(err){
+        if(err){
+            console.log(err);
+            res.send(err);
+        }
+        else
+        {
+            res.send("Successfully added a new article.")
+        }
+    });
 })
+.delete(function(req,res){
+    Article.deleteMany(function(err){
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.send("deleted all articles.");
+        }
+    });
+});
 
-app.post("/article", function(req,res){
 
+
+//REQUESTS TARGETING SPECIFIC ARTICLE
+app.route("/article/:item") 
+.get(function(req,res){
+    
+    Article.findOne({title: req.params.item},function(err, article_item){
+        if(err)
+        {
+            res.send("no specific article found.");
+        }
+        else
+        {
+            res.send(article_item);
+        }
+    });
 })
+.post(function(req,res){})
+.put(function(req,res){
+    Article.updateOne({title: req.params.item}, {$set: {title: req.body.title, content: req.body.content}}, function(err){
+        if(!err)
+        {
+            res.send("successfully updated article");
+            
+        }
+        else
+        {
+            res.send("failed to update.");
+        }
+    });
+})
+.patch(function(req,res){
+    Article.updateOne({title: req.params.item}, {$set: req.body}, function(err){
+        if(!err)
+        {
+            res.send("Successfully patched.")
+        }
+        else
+        {
+            res.send("failed to patch");
+        }
+    });
+})
+.delete(function(req,res){
+    Article.deleteOne({title: req.params.item}, function(err){
+        if(!err)
+        {
+            res.send("successfully deleted");
+        }
+        else
+        {
+            res.send("error, couldn't delete");
+        }
+    });
+});
 
 
 //ROUTING END
